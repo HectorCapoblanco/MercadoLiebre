93eee71f5fb0c9f2ad0f0dc9040217589d8320f5
HECTORCAPOBLANCO <cancer18ar@hotmail.com>
2022-06-30 00:10:09 -0300

    archivos linkeados

--- app.js
+++ app.js
@@ -7,3 +7,10 @@ app.use(express.static("public"));
 app.listen (3000,()=>{console.log("El servidor esta corriendo")});
 
 app.get('/',(req,res)=>{res.sendFile(path.resolve(__dirname,'./views/home.html'))})
+
+app.post('/',(req,res)=>{res.sendFile(path.resolve(__dirname,'./views/home.html'))})
+
+app.get('/login',(req,res)=>{res.sendFile(path.resolve(__dirname,'./views/login.html'))})
+
+
+app.get('/register',(req,res)=>{res.sendFile(path.resolve(__dirname,'./views/register.html'))})
