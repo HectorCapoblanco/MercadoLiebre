c126166d63d785a72866a70b82ed25c4a2806956
HECTORCAPOBLANCO <cancer18ar@hotmail.com>
2022-07-06 22:28:15 -0300

    archivo

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app =express ();
 
 app.use(express.static("public"));
 
-app.listen (3000,()=>{console.log("El servidor esta corriendo")});
+app.listen (process.env.PORT || 3000,()=>{console.log("El servidor esta corriendo")});
 
 app.get('/',(req,res)=>{res.sendFile(path.resolve(__dirname,'./views/home.html'))})
 
